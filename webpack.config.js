8b06b8be8f3e981dd7e9e718dec721c5fb7752f7
Alexander Shpin <@lexhpin@gmail.com>
2022-05-19 14:29:04 -0500

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restaurant Landing',
+            template: './dist/index.html',
+        }),
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+}
