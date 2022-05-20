56e0ffc5c2c672455edd7126c9d0813a66a5e02d
Alexander Shpin <@lexhpin@gmail.com>
2022-05-20 12:40:54 -0500

    Writing out the structure of the page in js

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,16 @@
   \**********************/
 /***/ (() => {
 
+const content = document.querySelector('#content');
 
+
+const generatePage = () => {
+    const header = document.createElement('header');
+
+    content.appendChild(header);
+}
+
+generatePage();
 
 /***/ })
 
@@ -16,3 +25,4 @@
 /******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
 /******/ }
 ]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBOzs7QUFHQTtBQUNBOztBQUVBO0FBQ0E7O0FBRUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50X2xhbmRpbmcvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgY29udGVudCA9IGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoJyNjb250ZW50Jyk7XG5cblxuY29uc3QgZ2VuZXJhdGVQYWdlID0gKCkgPT4ge1xuICAgIGNvbnN0IGhlYWRlciA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2hlYWRlcicpO1xuXG4gICAgY29udGVudC5hcHBlbmRDaGlsZChoZWFkZXIpO1xufVxuXG5nZW5lcmF0ZVBhZ2UoKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 const content = document.querySelector('#content');
 
+
+const generatePage = () => {
+    const header = document.createElement('header');
+
+    content.appendChild(header);
+}
+
+generatePage();
