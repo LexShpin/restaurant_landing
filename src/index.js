02e258e2fe3ee4b48d3b01a986696bcfccda7539
lexshpin <lexshpin@gmail.com>
2022-05-20 16:16:23 -0600

    Wrote out the main structure for the main page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const content = document.querySelector('#content');
 
-
-const generatePage = () => {
+const generatePage = (() => {
     /*
         header
             h1
@@ -16,19 +15,40 @@ const generatePage = () => {
 
     const header = document.createElement('header');
     const heading = document.createElement('h1');
+
+    const headerButtons = document.createElement('nav');
+    const mainButton = document.createElement('button');
+    const menuButton = document.createElement('button');
+    const contactUsButton = document.createElement('button');
+
     const main = document.createElement('div');
     const menu = document.createElement('div');
     const contactUs = document.createElement('div');
+    
     const footer = document.createElement('footer'); 
 
+    // header and buttons to switch between tabs
     header.classList.add('header');
     heading.classList.add('heading');
-
-    heading.textContent = 'Hogsmeade';
-
+    heading.textContent = 'Star Wars Caffee';
     header.appendChild(heading);
+    
+    headerButtons.classList.add('header-buttons');
+    mainButton.classList.add('header__btn');
+    menuButton.classList.add('header__btn');
+    contactUsButton.classList.add('header__btn');
+
+    mainButton.textContent = 'Main';
+    menuButton.textContent = 'Menu';
+    contactUsButton.textContent = 'Contact Us';
+
+    headerButtons.appendChild(mainButton);
+    headerButtons.appendChild(menuButton);
+    headerButtons.appendChild(contactUsButton);
+    header.appendChild(headerButtons);
+
+    // Displaying main content
+    
 
     content.appendChild(header);
-}
-
-generatePage();
+})();
---
+++ src/main.js
@@ -0,0 +1,9 @@
+const main = () => {
+    const mainContent = document.createElement('div');
+    const mainContentHeading = document.createElement('h1');
+    const mainContentText = document.createElement('p');
+
+    mainContentHeading.textContent = 'About';
+}
+
+export default main;
